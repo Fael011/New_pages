7132f35dba9835ae24964569a89f6819a5e6b09b
EA - Rafael Lima Viana <lima_viana@educadventista.org>
2024-04-11 22:15:28 -0300

    inicio da utilização do grid

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const botaoAbrir = document.querySelector(".HeaderMobile > button")
-const botaoFechar = document.querySelector(".HeaderMobile nav button")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
-const nav = document.querySelector(".HeaderMobile > nav")
+const nav = document.querySelector("header .mobile")
 
 botaoAbrir.addEventListener('click', abrirMenu)
 botaoFechar.addEventListener('click', fechareMenu)
@@ -16,4 +16,3 @@ function fechareMenu(){
     body.classList.remove("escurecer")
     nav.classList.remove("openMenu")
 }
-
